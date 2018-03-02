eb99bd380280195d2644b86451ebb4f6ba16113e
pingzhen <pingzhen@yiihua.com>
2018-03-02 16:12:28 +0800

    develop

--- nodetest/this_test.js
+++ nodetest/this_test.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 /*
 * 不同的场景，this的指向不同
@@ -7,23 +7,60 @@
 * 方法调用
 * 调用构造函数
 * 显式的设置this
+* 严格模式／非严格模式 new 显式函数／匿名函数／箭头函数  回调函数
+* http://blog.csdn.net/w_vc_love/article/details/51012252
 ****************************************************
 * 在全局作用域中出现的this指全局对象，浏览器中是window对象
 * this当前调用这个方法的对象
 ****************************************************
 * apply  call   bind
 */
-var foo = function() {
-    var myName = "Foo Module";
+//var foo = function() {
+//    var myName = "Foo Module";
+//
+//    function sayhello() {
+//        console.log(myName);
+//    };
+//
+//    this.bar = function() {
+//        setTimeout(sayhello, 1000);
+//    };
+//};
+//
+//var f = new foo;
+//f.bar();
 
-    function sayhello() {
-        console.log(myName);
-    };
+//exports.haha = 999;
+//console.log(1111111111);
+//console.log(this);
+//var a = {
+//    c: 8,
+//    sitt: () => {
+//        console.log(this);
+//        return this.c;    
+//    }
+//};
+//console.log(this);
+//console.log(2222222222);
+//a.sit = function () {
+//    console.log(this);
+//    return this.c;
+//};
+//
+//let d = a.sitt();
+//console.log(d);
 
-    this.bar = function() {
-        setTimeout(sayhello, 1000);
-    };
-};
 
-var f = new foo;
-f.bar();
+function fn(){
+    function fn2(){
+        console.log(this);
+        this.age = 18;
+    }
+    //fn2();
+    //console.log(this); //global
+    //console.log(this.age); //18
+    //console.log(global.age); //18
+    var cc = new fn2();
+    console.log(cc.age);
+}
+fn();
